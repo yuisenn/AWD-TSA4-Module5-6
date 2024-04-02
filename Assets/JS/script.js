9a97cce30ce605755d03d6e8ad197074d0a33bd7
yuisenn <157201702+yuisenn@users.noreply.github.com>
2024-04-03 00:48:37 +0800

    Update script.js

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,16 +1,13 @@
-// script.js
-
-// Get the modal element
 var modal = document.getElementById("modal");
 
-// Function to display the modal with dish details
+
 function showModal(dishName, description) {
     var modalContent = document.getElementById("modal-content");
     modalContent.innerHTML = "<h2>" + dishName + "</h2><p>" + description + "</p>";
     modal.style.display = "block";
 }
 
-// Function to hide the modal
+
 function hideModal() {
     modal.style.display = "none";
 }
